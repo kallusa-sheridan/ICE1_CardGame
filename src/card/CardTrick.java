7b53fe7cbaa6e830801a63c0c9ac4cde4694794e
kallusa <kallusa00@gmail.com>
2026-01-21 22:26:40 -0500

    Implemented random hand and user search

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,11 +4,14 @@
  */
 package card;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author srinivsi
+ * @modifier kallusa@sheridancollege.ca
  */
 public class CardTrick {
     
@@ -20,13 +23,38 @@ public class CardTrick {
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue((int)(Math.random() * 13 + 1));
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[(int)(Math.random() * 4)]);
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        Scanner k = new Scanner(System.in);
+        Card userCard = new Card();
+        
+        System.out.print("Enter a card value (1-13): ");
+        userCard.setValue(k.nextInt());
+        
+        System.out.print("Enter a suit (0-3 where 0=Hearts, 1=Diamonds, 2=Spades, 3=Clubs): ");
+        int suitIndex = k.nextInt();
+        userCard.setSuit(Card.SUITS[suitIndex]);
+        
         // and search magicHand here
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == userCard.getValue() && card.getSuit().equalsIgnoreCase(userCard.getSuit())) {
+                found = true;
+                break;
+            }
+        }
+        
         //Then report the result here
-        // add one luckcard hard code 2,clubs
+        if (found) {
+            System.out.println("Congratulations! Your card is in the magic hand.");
+        } else {
+            System.out.println("Sorry, your card is not in the magic hand.");
+        }
     }
     
 }
